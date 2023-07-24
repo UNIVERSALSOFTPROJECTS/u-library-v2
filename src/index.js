2f9b48fb38a784cfa1c2bcc2d57125106c629795
brian <unasbrian@gmail.com>
2023-07-24 11:46:55 -0500

    fix

--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
 export {default as Hello} from "./lib/Hello.svelte";
-export {default as Header} from "./lib/Header.svelte";
-export {default as Login} from "./lib/modals/Login.svelte";
+//export {default as Header} from "./lib/Header.svelte";
+//export {default as Login} from "./lib/modals/Login.svelte";
