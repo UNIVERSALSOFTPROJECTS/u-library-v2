45a3a22c31f709d7feab91a1351f68d32145d96a
SteveVR <stevevaca2020@gmail.com>
2023-08-21 18:22:00 -0500

    correcion de commit de HEAD perdidios

--- src/lib/input/InputPassword.svelte
+++ src/lib/input/InputPassword.svelte
@@ -1,18 +1,9 @@
 <script>
     export let password;
 	let showPassword = false;
-<<<<<<< HEAD
-	const dataPassword=(event) =>{ password = event.target.value }
-	const togglePasswordHide = () => { showPassword = !showPassword }
-</script>
-
-<input class="ipt" type={showPassword ? 'text' : 'password'}  on:input={dataPassword} placeholder="Contraseña" autocomplete="off">
-<button type="button" on:click={togglePasswordHide} class="btn {showPassword ? 'no-eye' : 'eye'}"></button>
-=======
 	const dataPassword = (e) =>{ password = e.target.value }
 	const togglePasswordHide = () => { showPassword =! showPassword }
 </script>
 
 <input class="ipt" type={showPassword ? 'text' : 'password'}  placeholder="Contraseña" autocomplete="off" on:input={dataPassword}>
 <button type="button" class="btn {showPassword ? 'no-eye' : 'eye'}" on:click={togglePasswordHide}></button>
->>>>>>> a2dc001 (modal de depoosito listo)
