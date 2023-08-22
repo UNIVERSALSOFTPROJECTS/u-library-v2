ac44dfd521b1b6e12565204de1c4bb72d7c409a5
SteveVR <stevevaca2020@gmail.com>
2023-08-22 16:05:20 -0500

    se agrego componenet server connection

--- src/index.js
+++ src/index.js
@@ -2,3 +2,4 @@ export {default as Hello} from "./lib/Hello.svelte";
 //export {default as Header} from "./lib/Header.svelte";
 export {default as Login} from "./lib/modals/Login.svelte";
 export {default as Deposit} from "./lib/payments/Deposit.svelte";
+export {default as ServerConnection} from "./js/server";
