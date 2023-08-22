f80c75346e2ca715c6cd0ef3867f1620f4650e70
SteveVR <stevevaca2020@gmail.com>
2023-08-22 18:07:06 -0500

    se agrego el notifier

--- src/index.js
+++ src/index.js
@@ -3,3 +3,4 @@ export {default as Hello} from "./lib/Hello.svelte";
 export {default as Login} from "./lib/modals/Login.svelte";
 export {default as Deposit} from "./lib/payments/Deposit.svelte";
 export {default as ServerConnection} from "./js/server";
+export {default as Notifier} from "./lib/Notifier.svelte";
