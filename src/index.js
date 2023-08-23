fdc0f4ed7f3d89d28cdc641d947cf00085154972
SteveVR <stevevaca2020@gmail.com>
2023-08-23 18:48:27 -0500

    actualizacion de registro mixto , con correciones nuevamente

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-export {default as Hello} from "./lib/Hello.svelte";
+
 //export {default as Header} from "./lib/Header.svelte";
 export {default as Login} from "./lib/modals/Login.svelte";
 export {default as Deposit} from "./lib/payments/Deposit.svelte";
 export {default as ServerConnection} from "./js/server";
 export {default as Notifier} from "./lib/Notifier.svelte";
 export {default as Modal} from "./lib/Modal.svelte";
-export {default as SingupW} from "./lib/modals/SingupW.svelte";
+export {default as SingupW} from "./lib/modals/SingupM.svelte";
