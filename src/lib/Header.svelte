a03cc1e801d798f6ce63b81cf0cbd32a689a6ba1
SteveVR <stevevaca2020@gmail.com>
2023-08-23 10:26:20 -0500

    cambios para evitar evitar error en el login parte 2

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -15,6 +15,8 @@
     //DEPOSITOS MONTOS FAVORITOS
     //export let amountsFav = [5000, 10000, 30000, s50000];
     export let amountsFav = [50, 100, 300, 500];
+    let userState = "logout";
+    let active_view = "home";
     //export let ASSETS_GLOBAL;
     let loginModalOpen = false;
     let signupModalOpen = false;
@@ -33,6 +35,7 @@
         user = user_;
         notify = await utils.showNotify("success","Bienvenido a "+platform);
         loginModalOpen = false;
+        userState = "loggedIn"
     }
     const onLoginError = async (error)=>{
         notify = {};
