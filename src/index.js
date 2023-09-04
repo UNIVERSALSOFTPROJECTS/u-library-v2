ebe87787b921a64cd064a8f5e9ee967381102331
SteveVR <stevevaca2020@gmail.com>
2023-09-04 12:47:45 -0500

    subiendo utils js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@
 export {default as Login} from "./lib/modals/Login.svelte";
 export {default as Deposit} from "./lib/payments/Deposit.svelte";
 export {default as ServerConnection} from "./js/server";
+export {default as Utils} from "./js/util";
 export {default as Notifier} from "./lib/Notifier.svelte";
 export {default as Modal} from "./lib/Modal.svelte";
 export {default as SingupW} from "./lib/modals/SingupM.svelte";
