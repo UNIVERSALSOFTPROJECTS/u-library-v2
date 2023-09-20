23f0f4e42d425ba3149cc3a2dbde63f063eae1e9
Dayana Horna <dayisitax@gmail.com>
2023-09-20 10:55:12 -0500

    fix

--- src/lib/input/InputPassword.svelte
+++ src/lib/input/InputPassword.svelte
@@ -1,10 +1,11 @@
 <script>
     export let password;
-
+	export let loginEnter;
+	
 	let showPassword = false;
 	const dataPassword = (e) =>{ password = e.target.value }
 	const togglePasswordHide = () => { showPassword =! showPassword }
 </script>
 
-<input class="ipt" type={showPassword ? 'text' : 'password'}  placeholder="Contraseña" autocomplete="new-password"  on:input={dataPassword}>
+<input class="ipt" type={showPassword ? 'text' : 'password'}  placeholder="Contraseña" autocomplete="new-password" on:keypress={loginEnter} on:input={dataPassword}>
 <button type="button" class="btn {showPassword ? 'no-eye' : 'eye'}" name="passowrd" on:click={togglePasswordHide}></button>
