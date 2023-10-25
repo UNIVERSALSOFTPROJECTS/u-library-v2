4ecaf9ab42e53d377e4ec8c4ba4daac2f2d873af
Dayana Horna <dayisitax@gmail.com>
2023-10-25 15:18:59 -0500

    fix recover pass

--- src/lib/topbar/RecoverPassword.svelte
+++ src/lib/topbar/RecoverPassword.svelte
@@ -6,25 +6,32 @@
   export let open;
 
   let forgotPass = {}
+  let isEmail;
 
   onMount(() => {});
 
   const closeModal = () => {
-    console.log("cerrando");
     open = false;
   };
 
   
 
   const recoverPass = async() => {
+    if(!forgotPass.email) return notify.error("Ingrese su correo");
+    if (!isEmail) return notify.error("Su correo tiene el formato incorrecto");
     try {
         let payload = {...forgotPass}
-        await backend.u_user.recoverPassword(payload)
         notify.success("Un mensaje esta siendo enviado a su correo electronico")
+        await backend.u_user.recoverPassword(payload)
     } catch (error) {
         notify.error("Surgio un error, contacte a soporte");
     }
     open = false;
+  }
+
+  const validateEmail = async() => {
+    isEmail = /[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/.test(forgotPass.email);
+    if (!isEmail) return notify.error("Su correo tiene el formato incorrecto")
   }
 </script>
 
@@ -35,8 +42,8 @@
         <button class="btn close" on:click={closeModal} />
     </div>
     <div class="recoverpass__body">
-        <input bind:value={forgotPass.email} class="ipt" type="email" placeholder="Ingrese su correo electronico" />
-        <p>Al correo ingresado se le enviara un mensaje con la opcion de cambiar su clave.
+        <input bind:value={forgotPass.email} on:blur={validateEmail} class="ipt" type="email" placeholder="Ingrese su correo electronico" />
+        <p>Ingrese el correo correspondiente, proocederemos a enviar un mensaje con la finalidad de cambiar su clave.
             <br>Verificar que su correo se el correcto.
         </p>
         <button on:click={recoverPass} class="btn recoverpass--btn">Enviar</button>
