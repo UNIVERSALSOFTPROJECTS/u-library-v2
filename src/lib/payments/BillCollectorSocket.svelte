fdb7690db033c2739a8b1b176042d88881cf5817
brian <unasbrian@gmail.com>
2024-01-03 12:38:02 -0500

    fix billCollector

--- src/lib/payments/BillCollectorSocket.svelte
+++ src/lib/payments/BillCollectorSocket.svelte
@@ -1,17 +1,18 @@
 <script>
     import { onMount } from "svelte";
     export let user;
-
-    //import { Client } from '@stomp/stompjs';
-
-    //let stompClient;
+    export let onMessage;
     let socket;
-    let connnected;
     const brokerURL="ws://localhost:8181";
+    const log="BillCollectorSocket:";
 
+    let reconnectingAttempts;
+    let connnected="no-init";
+    let statusMessage = {open:'Conectado', reconnecting:'Reconectando', close:'Desconectado' };
+    
     $:{//escuchara cambios en table user.
-        if(user !== undefined && user.token){
-            console.log("USERSSSSSSSS", user);
+        if(user !== undefined && user && user.token){
+            console.log(`${log} User Change`, user);
             connectUserToBillCollector(user);
         }
     }
@@ -19,70 +20,82 @@
     const connectUserToBillCollector=(user)=>{
         let playerId = user.playerId;
         let token = user.token;
-        console.log("Socket", socket);
-        //stompClient.publish({destination:"/", body:{playerId, token} });
-        socket.send(JSON.stringify({playerId, token}));
+        //if(!socket) return alert("No hay conexion con Billetero.");
+        if(socket && socket.readyState  === WebSocket.OPEN ){
+            socket.send(JSON.stringify({playerId, token}));
+        }else{//Closing, closed, CONNECTING
+            console.log(`${log} Waiting 1 sec for send.`);
+            setTimeout( ()=>{ connectUserToBillCollector(user); }, 2000 );
+        }
+        
     }
 
     const startSocket=()=>{
-
         socket = new WebSocket(brokerURL);
-
+        /*socket.onopen=(event)=>{ }*/
         socket.addEventListener("open", (event) => {
             console.log("conectado a BillCollector");
-            connnected = true;
-            // Enviar datos al servidor después de la conexión
-            //const Tokens = { Token, Player_id };
-            //socket.send(JSON.stringify(Tokens));
-
-            
+            connnected = 'open';
+            reconnectingAttempts=0;
         });
 
         socket.addEventListener("message", (event) => {
             console.log("mensaje ",event.data);
-        });
+            onMessage(event.data);
+        }); 
 
         socket.addEventListener("close", (event) => {
-            console.log("cerrado");
+            console.log("BillCollecctoSocket: closed!");
+            const attempts = 10; 
+            connnected = 'close';
+            if( user && user.token){
+                if(reconnectingAttempts<attempts){
+                    connnected = 'reconnecting';
+                    reconnectingAttempts++;
+                    setTimeout(()=>{ console.log("BillCollecctoSocket: Reconectando..."); startSocket(); }, 2*1000);
+                }else{
+                    alert(`${attempts} intentos de reconexion fallida. Para intentar conectar nuevamente actualice sistema.`);
+                }
+            }
+            
         });
 
-    // Manejar errores
     socket.addEventListener("error", (event) => {
-      //logMessage(Error: ${event.message});
-      console.log("Error", event);
+        connnected = 'error';
+      console.log(`${log} ERROR al conectar socket`, event);
+
     });
 
-
-
-
-        /*stompClient = new Client({
-                    brokerURL,
-                    connectHeaders: {},
-                    debug: function (str) {console.log(str);},
-                    reconnectDelay: 2500,
-            });
-
-        stompClient.onConnect = (frame) => {
-            stompClient.subscribe('/newBill', (data) => {
-                    console.log("newBill",JSON.parse(data.body));
-                    //EventManager.publish("changeBalance",JSON.parse(data.body) )
-
-            });
-        };
-
-        stompClient.onWebSocketError = (error) => {
-            console.error('Error with BillCollector Websocket', error);
-        };
-
-        stompClient.onStompError = (frame) => {
-            console.error('Broker reported error: ' + frame.headers['message']);
-            console.error('Additional details: ' + frame.body);
-        };
-
-        stompClient.activate();*/
     }
 
+
     onMount( ()=>{
+        reconnectingAttempts=0;
         startSocket();
     });
 </script>
+<div id="bill-notify" class={'status-'+connnected}> Billetero {statusMessage[connnected]} {reconnectingAttempts && reconnectingAttempts<10?`... (${reconnectingAttempts})`:''} </div>
+
+<style>
+    div#bill-notify{
+        position: absolute;
+        bottom:1px;
+        right:1px;
+        padding:3px;
+        width:200px;
+        border-radius: 2px;
+    }
+    .status-open{
+        background:#44ed7cb2;
+    }
+    .status-close{
+        background:#ee3333b2;
+    }
+    .status-reconnecting{
+        background:#c2b65bb2;
+    }
+    .status-no-init{
+        display: none;
+    }
+    
+</style>
