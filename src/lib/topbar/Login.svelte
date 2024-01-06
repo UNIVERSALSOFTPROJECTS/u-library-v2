a3651048f7b3938f78488d095089ad8fa7af240e
brian <unasbrian@gmail.com>
2024-01-06 10:44:07 -0500

    fix

--- src/lib/topbar/Login.svelte
+++ src/lib/topbar/Login.svelte
@@ -130,7 +130,7 @@
         <p>{t("login.access")}</p>
       {/if}
     </button>
-    <button on:click={onOpenRecoverPass} class="btn link">{t("login.forgetPassword")}</button>
+    <button on:click={onOpenRecoverPass} class="btn- link">{t("login.forgetPassword")}</button>
 
   </div>
 </div>
