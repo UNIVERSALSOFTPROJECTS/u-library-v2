e99d0e12400720823539d4ae693ac4f14b8a060b
Dayana Horna <dayisitax@gmail.com>
2024-02-16 12:46:38 -0500

    socket

--- src/js/SocketConnector.js
+++ src/js/SocketConnector.js
@@ -4,9 +4,10 @@ import { Client } from '@stomp/stompjs';
 const SocketConnector = (() => {
 
     let stompClient
-    let conf ={};
+    let conf = {};
 
-    function connectToLobbySocket(user, conf){
+    function connectToLobbySocket(user, conf) {
+        console.log("CONF", conf);
         console.log(`Opening WS connection with lobbybff`);
         stompClient = new Client({
             brokerURL: conf.WS_URL,
@@ -46,30 +47,30 @@ const SocketConnector = (() => {
 
     }
 
-    function setConfig(conf_){
+    function setConfig(conf_) {
         conf = conf_;
     }
-       
-    function connect(user){
-       console.log(`Opening WS connection with   ${conf.BALANCE_WS}/change-balance`)
 
-            stompClient = new Client({
-                    brokerURL:conf.BALANCE_WS+'/change-balance',
-                    connectHeaders: {
-                         platformId: user.platformId,
-                         userId: user.id,
-                         currencyISO:user.currency,
-                         connectionId:user.token
-                    },
-                    debug: function (str) {console.log(str);},
-                    reconnectDelay: 2500,
-            });
+    function connect(user) {
+        console.log(`Opening WS connection with   ${conf.BALANCE_WS}/change-balance`)
+
+        stompClient = new Client({
+            brokerURL: conf.BALANCE_WS + '/change-balance',
+            connectHeaders: {
+                platformId: user.platformId,
+                userId: user.id,
+                currencyISO: user.currency,
+                connectionId: user.token
+            },
+            debug: function (str) { console.log(str); },
+            reconnectDelay: 2500,
+        });
 
         stompClient.onConnect = (frame) => {
             stompClient.subscribe('/users/queue/balance', (data) => {
-                    console.log("gretting",JSON.parse(data.body));
-                    new Audio('https://assetsapiusoft.s3.us-west-2.amazonaws.com/audios/iphone-notificacion.mp3').play();
-                    EventManager.publish("changeBalance",JSON.parse(data.body) )
+                console.log("gretting", JSON.parse(data.body));
+                new Audio('https://assetsapiusoft.s3.us-west-2.amazonaws.com/audios/iphone-notificacion.mp3').play();
+                EventManager.publish("changeBalance", JSON.parse(data.body))
 
             });
         };
