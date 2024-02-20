794cd0d33f0360edd02ceef414d853abf973ce38
Ncerna <nimerce1234@gmail.com>
2024-02-20 18:05:37 -0500

    add contol btn printer

--- src/lib/topbar/Configs.svelte
+++ src/lib/topbar/Configs.svelte
@@ -4,9 +4,10 @@
  
 </script>
 
-<div>
+<div >
   {#if configsAllowed.billCollector}
-  <button on:click={()=>onShowCustomConfig('billColector')} class="btn config--btn">Billetero</button>
+  <button on:click={()=>onShowCustomConfig('billColector')} class="btn config--btn" style="margin: 5px;">Billetero</button>
+  <button on:click={()=>onShowCustomConfig('printer')} class="btn config--btn" style="margin: 5px;">Impresora</button>
   {/if}
 
 </div>
