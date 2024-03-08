01df5ad900663631e045a339dd19944c3f20a4b5
Dayana Horna <dayisitax@gmail.com>
2024-03-08 09:47:21 -0500

    format sportbook

--- src/lib/pages/SportbookPage.svelte
+++ src/lib/pages/SportbookPage.svelte
@@ -1,30 +1,52 @@
 <script>
   import { onDestroy, onMount } from "svelte";
+  import ut from '../../js/util';
 
   export let userState;
-  export let sportbookGameUrl = "";
   export let active_view
   export let user;
   export let options;
   export let loginModalOpen;
   export let GAMEAPI_URL;
-  export let GAME_TOKEN;
-  export let getGameUrl
-  export let mode;
 
+  let sportbookGameUrl = '';
+  let mode = ut.isMobile() ? "mb" : "wb";
   let deviceiframe = mode;
   let page = active_view == "sportbooklive" ? "live" : "sport";
 
   const edg_id = "8042022_digitain";
   const wt_id = "wintech_gaming";
   const nvb_id = "novusbet";
-  const baseUrlDigtain = `${GAMEAPI_URL}/e-digtain/init?t=-&lang=es&gameid=8042022_digitain&m=${deviceiframe}`;
+  const guestURLdigtain = `${GAMEAPI_URL}/e-digtain/init?t=-&lang=es&gameid=${edg_id}&m=${deviceiframe}`;
   const baseUrlWintech ='https://betslip.sportsapi.la/mainbk/betslip';
   const baseUrlNovusbet = `https://www.3p.latinsport21.net/${page}?lang=es-ES`;
+  const games = {
+    'digtain':{
+      provider: "edg",
+      brand: "Digtain Sports Book",
+      gameid: edg_id,
+      mode,
+    },
+    'wintech':{
+      gameid: wt_id,
+      provider: "wt",
+      brand: "WinTech",
+      mode,
+      page:active_view
+    },
+    'novusbet':{
+      gameid: nvb_id,
+      provider: "nvb",
+      brand: "Novus Bet",
+      mode
+    }
+
+  }
   
   onMount(()=>{
     window.addEventListener("message", receiveMessage, false);
     document.body.style.overflow="hidden";
+    console.log("openSport1:", options, options.gameid);
   });
 
   $: {
@@ -32,9 +54,9 @@
   }
 
   async function openSport() {
-    if (options.gameid == edg_id)openDigtainSportbook();
-    else if (options.gameid == wt_id) openWintechSportbook();
-    else if (options.gameid == nvb_id) openNovusbetSportbook();
+    if (options.gameid == edg_id)openDigtain();
+    else if (options.gameid == wt_id) openWintech();
+    else if (options.gameid == nvb_id) openNovusbet();
     console.log("openSport:", sportbookGameUrl);
   }
 
@@ -44,66 +66,25 @@
     }
   };
 
-  const openDigtainSportbook = async () => {
-
-    const game = {
-      provider: "edg",
-      brand: "Digtain Sports Book",
-      gameid: edg_id,
-      mode: mode,
-    };
-    
-    let url = baseUrlDigtain;
-    if (userState == "loggedIn") {
-    let gameToken = GAME_TOKEN;
-    console.log("gameToken:", gameToken);
-    url = getGameUrl;
-    if (active_view == "sportbook")url += active_view == "sportbook" ? "&currentgame=preMatch" : "";
-    else if (active_view == "sportbooklive")url += active_view == "sportbooklive" ? "&currentgame=live" : "";
-    if (options.eventInfo && mode == "wb")url += "&eventInfo=" + JSON.stringify(options.eventInfo);
-    else if (options.eventInfo && mode == "mb")url += "&eventInfo=" + JSON.stringify(options.eventInfo.Id);
-    } else {
-      if (active_view == "sportbooklive")url += active_view == "sportbooklive" ? "&currentgame=live" : "&currentgame=Home";
+  const openDigtain = async () => {
+    let url =userState == "loggedIn"? ut.getGameURL(GAMEAPI_URL, games.digtain, options.gameToken) : guestURLdigtain;
+    url += active_view == "sportbooklive" ? "&currentgame=live" : "&currentgame=PreMatch";
+    if(options.eventInfo){
+      const eventInfo=JSON.stringify(mode == "wb"?options.eventInfo:options.eventInfo.Id);
+      url += "&eventInfo=" +eventInfo;
     }
     sportbookGameUrl = url;
   };
 
-  const openWintechSportbook = async () => {
-    const game = {
-      gameid: wt_id,
-      provider: "wt",
-      brand: "WinTech",
-      mode: mode,
-      page:active_view
-    };
-    let url = baseUrlWintech;
-    if (userState == "loggedIn") {
-      let gameToken = GAME_TOKEN;
-      url = getGameUrl;
-      if (active_view == "sportbook")url += active_view == "sportbook" ? "&currentgame=preMatch" : "";
-      else if (active_view == "sportbooklive")url += active_view == "sportbooklive" ? "&currentgame=live" : "";
-    } else {
-      if (active_view == "sportbooklive")url += active_view == "sportbooklive" ? "&currentgame=live" : "&currentgame=Home";
-    }
+  const openWintech = async () => {
+    let url = userState == "loggedIn"? ut.getGameURL(GAMEAPI_URL, games.wintech, options.gameToken) : baseUrlWintech;
+    url += active_view == "sportbooklive" ? "&currentgame=live" : "&currentgame=PreMatch";
     sportbookGameUrl = url;
   };
 
-  const openNovusbetSportbook = async () => {
-    const game = {
-      gameid: nvb_id,
-      provider: "nvb",
-      brand: "Novus Bet",
-      mode: mode,
-    };
-    let url = baseUrlNovusbet;
-    if (userState == "loggedIn") {
-      let gameToken = GAME_TOKEN;
-      url = getGameUrl;
-      if (active_view == "sportbook")url += active_view == "sportbook" ? "&currentgame=preMatch" : "";
-      else if (active_view == "sportbooklive")url += active_view == "sportbooklive" ? "&currentgame=live" : "";
-    } else {
-      if (active_view == "sportbooklive")url += active_view == "sportbooklive" ? "&currentgame=live" : "&currentgame=Home";
-    }
+  const openNovusbet = async () => {
+    let url = userState == "loggedIn"? ut.getGameURL(GAMEAPI_URL, games.novusbet, options.gameToken) : baseUrlNovusbet;
+    url += active_view == "sportbooklive" ? "&currentgame=live" : "&currentgame=PreMatch";
     sportbookGameUrl = url;
   };
   
--- stories/SportbookPage.stories.js
+++ stories/SportbookPage.stories.js
@@ -1,4 +1,4 @@
-import TopBar from '../src/lib/topbar/TopBar.svelte';
+
 import SportbookPage from '../src/lib/pages/SportbookPage.svelte'
 import init from './init';
 init.start();
@@ -8,20 +8,16 @@ let userState = 'logout';
 let active_view = null;
 let user = {};
 let loginModalOpen = false;
-let sportbookGameUrl = "";
 let options = {
-  gameid: 'novusbet'
+  gameid: 'wintech_gaming'
 };
 let GAMEAPI_URL = 'https://apiuniversalsoft.com/api';
-let GAME_TOKEN = null;
-let getGameUrl = null;
-let mode = 'web';
 let modalOpened;
 
 
 
 export default {
-  title: 'SportbookPage_',
+  title: 'SportbookPage',
   component: SportbookPage,
 };
 
@@ -32,7 +28,7 @@ export default {
 
 export const SportbookPage_ = () => ({
   Component: SportbookPage,
-  props: { user, active_view, modalOpened, loginModalOpen, userState, sportbookGameUrl, options, GAMEAPI_URL, GAME_TOKEN, getGameUrl, mode }
+  props: { user, active_view, modalOpened, loginModalOpen, userState, options, GAMEAPI_URL }
 });
 
 
