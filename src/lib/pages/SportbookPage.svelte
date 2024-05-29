2b3cd99f68f8111c2b596d1e46f7b1a40140e3ea
Dayana Horna <dayisitax@gmail.com>
2024-05-29 09:30:08 -0500

    feat: added new sports configuration

--- src/lib/pages/SportbookPage.svelte
+++ src/lib/pages/SportbookPage.svelte
@@ -22,6 +22,7 @@
   const edg_id = "8042022_digitain";
   const wt_id = "wintech_gaming";
   const nvb_id = "novusbet";
+  const bbq_id = "sport-betbuq"
   const guestURLdigtain = `${GAMEAPI_URL}/e-digtain/init?t=-&gameid=${edg_id}&m=${deviceiframe}&skin=generic&`;
   const baseUrlWintech ='https://betslip.sportsapi.la/mainbk/betslip';
   const baseUrlNovusbet = `https://www.3p.latinsport21.net/${page}?lang=es-ES`;
@@ -30,6 +31,12 @@
       provider: "edg",
       brand: "Digtain Sports Book",
       gameid: edg_id,
+      mode,
+    },
+    'bbq':{
+      provider: "bbq",
+      brand: "BetBuq",
+      gameid: bbq_id,
       mode,
     },
     'wintech':{
@@ -70,9 +77,21 @@
     if (options.gameid == edg_id)openDigtain();
     else if (options.gameid == wt_id) openWintech();
     else if (options.gameid == nvb_id) openNovusbet();
+    else if (options.gameid == bbq_id) openBBQ();
   }
 
-   const openDigtain = async () => {
+  const openBBQ = async () => {
+    let url =userState == "loggedIn"? ut.getGameURL(GAMEAPI_URL, games.bbq, options.gameToken) : guestURLdigtain;
+    url += active_view == "sportbooklive" ? "&page=live" : "&page=sport";
+    url += `&lang=${lang}`;
+    if (userState == "loggedIn"){
+      const {data} = await backend.game.getURLNovus(url);
+      url = data.url
+    }
+   sportbookGameUrl = url;
+  }
+
+  const openDigtain = async () => {
     let url =userState == "loggedIn"? ut.getGameURL(GAMEAPI_URL, games.digtain, options.gameToken) : guestURLdigtain;
     url += active_view == "sportbooklive" ? "&currentgame=live" : "&currentgame=PreMatch";
     url += `&lang=${lang}`;
