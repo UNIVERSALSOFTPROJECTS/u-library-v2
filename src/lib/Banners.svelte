abf7d862919aad368c47d3f861c2b143cc6e814a
SteveVR <stevevaca2020@gmail.com>
2024-06-19 09:42:00 -0500

    update test component banners

---
+++ src/lib/Banners.svelte
@@ -0,0 +1,40 @@
+<script>
+    let banners = [
+                    {
+                        "page": "Coliseosport",
+                        "banners":[
+                        {
+                            "country":"cl",
+                            "banners":[
+                            {"url_w":"/Coliseosport/bn_w_apuesta.png"      ,"url_m":"/Coliseosport/bn_m_apuesta.png"         ,"dateFrom": "",           "dateUntil": "",           "goSection":""},
+                            {"url_w":"/Coliseosport/banner_sport_v2.png"   ,"url_m":"/Coliseosport/banner_sport_v2_mb.png"   ,"dateFrom": "",           "dateUntil": "",           "goSection":"sportbook"},
+                            {"url_w":"/Latinsport21/bn_w_sportlive.png"    ,"url_m":"/Latinsport21/bn_m_sportlive.png"       ,"dateFrom": "",           "dateUntil": "",           "goSection":"sportbooklive"},
+                            {"url_w":"/Coliseosport/banner_cashout_2.png"  ,"url_m":"/Coliseosport/banner_cashout_2_mb.png"  ,"dateFrom": "",           "dateUntil": "",           "goSection":"sportbook"},
+                            {"url_w":"/Coliseosport/banner_recharge_4.png" ,"url_m":"/Coliseosport/banner_recharge_4_mb.png" ,"dateFrom": "",           "dateUntil": "",           "goSection":""},
+                            {"url_w":"/Latinsport21/bn_w_virtuales.png"    ,"url_m":"/Latinsport21/bn_m_virtuales.png"       ,"dateFrom": "",           "dateUntil": "",           "goSection":"virtual"},
+                            {"url_w":"/Latinsport21/bn_w_caballos2.png"    ,"url_m":"/Latinsport21/bn_m_caballos.png"        ,"dateFrom": "",           "dateUntil": "",           "goSection":"horses"},
+                            {"url_w":"/Coliseosport/bn_w_slotlive.png"     ,"url_m":"/Coliseosport/bn_m_slotlive.png"        ,"dateFrom": "",           "dateUntil": "",           "goSection":"slotlive"},
+                            {"url_w":"/Coliseosport/bn_w_crash.png"        ,"url_m":"/Coliseosport/bn_m_crash.png"           ,"dateFrom": "",           "dateUntil": "",           "goSection":"crash"},
+                            {"url_w":"/Coliseosport/bn_m_slot.png"         ,"url_m":"/Coliseosport/bn_w_slot.png"            ,"dateFrom": "",           "dateUntil": "",           "goSection":"slot"},
+                            {"url_w":"/generic_imgs/banners/prom1.png"     ,"url_m":"/generic_imgs/banners/prom1_mb.png"     ,"dateFrom": "",           "dateUntil": "",           "goSection":"slot"},
+                            {"url_w":"/generic_imgs/banners/prom2.png"     ,"url_m":"/generic_imgs/banners/prom2_mb.png"     ,"dateFrom": "",           "dateUntil": "",           "goSection":"slot"},
+                            {"url_w":"/Coliseosport/w_promo_clp.png"       ,"url_m":"/Coliseosport/m_promo_clp.png"          ,"dateFrom": "30-04-2024", "dateUntil": "15-06-2024", "goSection":"slot"},
+                            {"url_w":"/Coliseosport/w_promo2_clp.png"      ,"url_m":"/Coliseosport/m_promo2_clp.png"         ,"dateFrom": "05-06-2024", "dateUntil": "04-09-2024", "goSection":"slot"},
+                            {"url_w":"/Coliseosport/w_promo3_clp.png"      ,"url_m":"/Coliseosport/m_promo3_clp.png"         ,"dateFrom": "05-06-2024", "dateUntil": "04-09-2024", "goSection":"crash"},
+                            {"url_w":"/Coliseosport/w_promo4_clp.png"      ,"url_m":"/Coliseosport/m_promo4_clp.png"         ,"dateFrom": "05-06-2024", "dateUntil": "04-09-2024", "goSection":"crslotliveash"}
+                            ]  
+                        },
+                        {
+                            "country":"co",
+                            "banners":[]
+                        },
+                        {
+                            "country":"ec",
+                            "banners":[]
+                        }
+                        ]
+                    }
+                ];
+
+        console.log(banners);
+</script>
