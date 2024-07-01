a14bda9cef63c40c92206a571d1101961190fd02
Dayana Horna <dayisitax@gmail.com>
2024-07-01 18:33:01 -0500

    Change rute launch

--- src/js/util.js
+++ src/js/util.js
@@ -21,7 +21,7 @@ const utils = (() => {
       return `${assets}/us_img_brands/${game.brand}.png` 
     }
     const getGameURL=(gameapi_url,game, usertoken)=>{
-      let url = gameapi_url +`/launch?gameid=${game.gameid}&p=${game.provider}&b=${game.brand}&m=${game.mode}&sessionid=${usertoken}`;
+      let url = gameapi_url +`/launch?gameid=${game.gameid}&p=${game.provider}&b=${game.brand}&m=${game.mode}&sessionid=${usertoken}&game_multiple=true`;
       return url;
     }
     const isMobile= () => {//tengo una manera mas reducida de hacer esto
