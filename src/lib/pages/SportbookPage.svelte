99a5f2001854021bc5a3c9b4a1911c0c3a12b4df
Dayana Horna <dayisitax@gmail.com>
2024-07-22 19:01:44 -0500

    fix

--- src/lib/pages/SportbookPage.svelte
+++ src/lib/pages/SportbookPage.svelte
@@ -114,7 +114,7 @@
 
   const openBetsW3 = async () => { 
     let url =userState == "loggedIn"? ut.getGameURLTest(GAMEAPI_URL, games.BetW3, options.gameToken) : guestURLbetw3;
-    url += active_view == "sportbooklive" ? "&sport_view=live" : "sport_view=sport";
+    url += active_view == "sportbooklive" ? "&sport_view=live" : "&sport_view=sport";
     url += `&lang=${lang}&r=url`;
     const {data} = await backend.game.getURLNovus(url);
     url = data.url
