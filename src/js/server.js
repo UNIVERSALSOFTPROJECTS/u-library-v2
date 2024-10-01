ae2cfd51dbb68472efd951d6edd113128560bcd7
Dayana Horna <dayisitax@gmail.com>
2024-10-01 15:42:13 -0500

    UPDATE

--- src/js/server.js
+++ src/js/server.js
@@ -8,7 +8,7 @@ const ServerConnection = (() => {
 
     const setConfig = (config) => {
         conf = config;
-        headers = { "Content-Type": "application/json;charset=UTF-8", "clientAuth": conf.CLIENT_AUTH, "client": conf.CLIENT_CODE, "x-tenant": conf["x-tenant"], origin: conf.domain};
+        headers = { "Content-Type": "application/json;charset=UTF-8", "clientAuth": conf.CLIENT_AUTH, "client": conf.CLIENT_CODE, "x-tenant": conf["x-tenant"], "origin": conf.DOMAIN};
     }
 
 
