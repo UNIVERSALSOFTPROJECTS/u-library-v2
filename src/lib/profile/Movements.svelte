c62e630fdef72887f9dcf9ea8846f52bd77e31c6
Franco Piscoya <114585682+fr4ncopiscoya@users.noreply.github.com>
2024-10-01 12:52:55 -0500

    fix(movements): Se agrego nueva opcion y funcionalidad si es vista sportbook

--- src/lib/profile/Movements.svelte
+++ src/lib/profile/Movements.svelte
@@ -17,18 +17,23 @@
     export let t;
     export let typeView = "";
 
-    let filter = typeView === "machine" ? "BDR" : "TT";
+    const filterMap = {
+        machine: "BDR",
+        sportbook: "BAP",
+    };
+
+    let filter = filterMap[typeView] || "TT";
+    console.log("filter:", filter);
+    
     let loadMovements = false;
     let movements = [];
     let dateFrom = currentDate();
     let dateTo = currentDate();
     let heightModal;
-
-    // Inicializar los totales
     let totalMonto = 0;
 
     const getMovements = async (type) => {
-        let newFilter = /RF|TTW|AP|CB|DV|BN/.test(type);        
+        let newFilter = /RF|TTW|AP|CB|DV|BN/.test(type);
         try {
             loadMovements = true;
             let response;
@@ -44,44 +49,106 @@
                 error = t("msg.duplicatedSession");
             else error = t("msg.contactSupport");
             onError(error);
-        }finally {
+        } finally {
             loadMovements = false;
         }
     };
 
+    // const filterMovemets = async (type) => {
+    //     totalMonto = 0;
+    //     filter = type;
+
+    //     if (filter === "BDR") {
+    //         let totalDepositos = 0;
+    //         let totalRetiros = 0;
+
+    //         const retiros = await getMovements("RE");
+    //         const depositos = await getMovements("DE");
+
+    //         movements = [...retiros, ...depositos];
+
+    //         depositos.forEach((item) => {
+    //             totalDepositos += item.monto;
+    //         });
+
+    //         retiros.forEach((item) => {
+    //             totalRetiros += item.monto;
+    //         });
+
+    //         totalMonto = totalDepositos + totalRetiros;
+    //         totalMonto = formatNumber(totalMonto);
+    //     } else {
+    //         movements = await getMovements(filter);
+    //     }
+
+    //     const betsAll = {
+    //         TTW: (o) => /-WIN-|PREMIO/.test(o.tipo_operacion),
+    //         AP: (o) => /-BET-|AP/.test(o.tipo_operacion),
+    //         RF: (o) => /-REFUND-/.test(o.tipo_operacion),
+    //         CB: (o) => /CASHBACK/.test(o.tipo_operacion),
+    //         BN: (o) => /Bono/.test(o.tipo_operacion),
+    //         DV: (o) => /DEVOLUCION/.test(o.tipo_operacion),
+    //         BAP: (o) => /-WIN-|-BET-|-AP-|-PREMIO-/.test(o.tipo_operacion),
+    //     };
+
+    //     movements = betsAll[type] ? movements.filter(betsAll[type]) : movements;
+
+    //     console.log("movements: ", movements);
+
+    //     movements.map((m) => {
+    //         m.hora_reg_timezone = moment(m.lfecha * 1000)
+    //             .tz(timezone)
+    //             .format("YYYY-MM-DD HH:mm:ss");
+    //         m.opc = /-REFUND-/.test(m.tipo_operacion)
+    //             ? "RF"
+    //             : /-BET-|AP/.test(m.tipo_operacion)
+    //               ? "AP"
+    //               : /PREMIO/.test(m.tipo_operacion)
+    //                 ? "CWI"
+    //                 : m.opc;
+    //         m.type = /Bono/.test(m.tipo_operacion)
+    //             ? "BN"
+    //             : /DEVOLUCION/.test(m.tipo_operacion)
+    //               ? "RF"
+    //               : m.opc;
+    //         m.monto = formatNumber(m.monto);
+    //     });
+
+    //     if (/DE|RE|TTW|AP|BAP/.test(filter)) {
+    //         //Sumar los montos y obtener el total
+    //         console.log("filter?: ", filter);
+
+    //         movements.forEach((item) => {
+    //             totalMonto += stringToNumber(item.monto);
+    //         });
+    //         totalMonto = formatNumber(totalMonto.toFixed(2));
+    //     }
+    // };
+
     const filterMovemets = async (type) => {
         totalMonto = 0;
-        filter = type;        
+        filter = type;
 
         if (filter === "BDR") {
-            let totalDepositos = 0;
-            let totalRetiros = 0;
-
             const retiros = await getMovements("RE");
             const depositos = await getMovements("DE");
 
+            const totalDepositos = calculateMovementSum(depositos);
+            const totalRetiros = calculateMovementSum(retiros);
             movements = [...retiros, ...depositos];
 
-            depositos.forEach((item) => {
-                totalDepositos += item.monto;
-            });
+            totalMonto = totalDepositos - totalRetiros;
 
-            retiros.forEach((item) => {
-                totalRetiros += item.monto;
-            });
-
-            totalMonto = totalDepositos + totalRetiros;
             totalMonto = formatNumber(totalMonto);
         } else {
+            totalMonto = 0;
             movements = await getMovements(filter);
         }
 
         if (filter === "DE" || filter === "RE") {
-            //Sumar los montos y obtener el total
-            movements.forEach((item) => {
-                totalMonto += item.monto;
-            });
-            totalMonto = formatNumber(totalMonto.toFixed(2));
+            totalMonto = 0;
+            totalMonto = calculateMovementSum(movements);
+            totalMonto = formatNumber(totalMonto);
         }
 
         const betsAll = {
@@ -91,11 +158,11 @@
             CB: (o) => /CASHBACK/.test(o.tipo_operacion),
             BN: (o) => /Bono/.test(o.tipo_operacion),
             DV: (o) => /DEVOLUCION/.test(o.tipo_operacion),
+            BAP: (o) =>
+                /-WIN-|-BET-|-AP-|-PREMIO-|-REFUND-/.test(o.tipo_operacion),
         };
 
-        movements = betsAll[type]
-            ? movements.filter(betsAll[type])
-            : movements;        
+        movements = betsAll[type] ? movements.filter(betsAll[type]) : movements;
 
         movements.map((m) => {
             m.hora_reg_timezone = moment(m.lfecha * 1000)
@@ -115,6 +182,32 @@
                   : m.opc;
             m.monto = formatNumber(m.monto);
         });
+
+        if (/TTW|AP|RF/.test(filter)) {
+            totalMonto = calculateMovementSum(movements);
+            totalMonto = formatNumber(totalMonto);
+        }
+        if (/BAP/.test(filter)) {
+            const premio = movements.filter(betsAll["TTW"]);
+            const totalPremio = calculateMovementSum(premio);
+
+            const apuesta = movements.filter(betsAll["AP"]);
+            const totalApuesta = calculateMovementSum(apuesta);
+
+            const rechazado = movements.filter(betsAll["RF"]);
+            const totalRechazado = calculateMovementSum(rechazado);
+
+            totalMonto = totalApuesta + totalRechazado - totalPremio;
+            totalMonto = formatNumber(totalMonto);
+        }
+    };
+
+    const calculateMovementSum = (data) => {
+        let montoData = 0;
+        data.forEach((item) => {
+            montoData += parseFloat(item.monto);
+        });
+        return montoData;
     };
 
     const copyTicket = (id, reference) => {
@@ -150,9 +243,11 @@
             <option value="">Últimos 30 días</option>
             <option value="">Este mes</option> -->
             {#if typeView === "machine"}
-            <option value="BDR">{t("movements.BDR")}</option>
+                <option value="BDR">{t("movements.BDR")}</option>
+            {:else if typeView === "sportbook"}
+                <option value="BAP">{t("movements.BAP")}</option>
             {:else}
-            <option value="TT">{t("movements.all")}</option>
+                <option value="TT">{t("movements.all")}</option>
             {/if}
             <option value="TTW">{t("movements.awards")}</option>
             <option value="AP">{t("movements.bets")}</option>
