e1bbfad3e12a24e249f9e657e83a59aeac385fab
SteveVR <stevevaca2020@gmail.com>
2024-10-03 17:33:54 -0500

    prueba 1 para teclado text y number

--- src/lib/withdrawal/WithdrawalW.svelte
+++ src/lib/withdrawal/WithdrawalW.svelte
@@ -56,7 +56,7 @@
     }
 
     async function validateWithdrawal() {
-        if(amount == 0 || amount == undefined) return onError(t("withdrawal.amount0"));
+        if(amount == 0 || amount == undefined || amount == "" || amount == null) return onError(t("withdrawal.amount0"));
         if(amount < infoUser.retiro_min) return onError(t("withdrawal.min")+infoUser.retiro_min+" "+user.currency);
         if(amount > infoUser.retiro_max) return onError(t("withdrawal.max")+infoUser.retiro_max+" "+user.currency);
         if(amount > user.balance) return onError(t("withdrawal.lowBalance"));
@@ -78,7 +78,7 @@
             if(data.resp == "ok"){
                 await getUpdateBalance(user);
                 user = JSON.parse(sessionStorage.getItem("user"));
-                onOk("msg.withdrawalRequestSend");
+                onOk(t("msg.withdrawalRequestSend"));
             }else{
                 let error;
                 if (data.msg == " Revisar Credencial Invalid receiver in  transaction. Recipient has to be registered with CPS in order to receive funds. For more information call Customer Services on {0}.") {
@@ -95,12 +95,33 @@
         }
     }
 
+    let activeInput = null;
+    function agregarNumero(numero) {
+        console.log(numero,activeInput);
+        
+        if (activeInput === 1) {
+            amount += numero;
+        }else if (activeInput === 2) {
+            infoAccount.numero_cta += numero;
+        } else if (activeInput === 3) {
+            infoAccount.adicional += numero;
+        }
+     
+    }
+    function eliminarUltimoCaracter() {
+        // if (password.length > 0) {
+        //     password = password.slice(0, -1);
+        // }
+    }
+    function cleanPassword() {
+        // password = "";
+    }
+
+
     onMount(()=>{ 
         detectLockedDeposit();
         if (!isLocked) checkWithdrawal(); 
-    });
-    console.log("tipeView: ", );
-    
+    });    
 </script>
 <div class="modal-body {isLocked?'locked':''}">
     {#if isLocked}
@@ -125,7 +146,7 @@
             {:else}
                 <div class="withdrawal__amount">
                     <b>{user.currency}</b>
-                    <input type="number" class="ipt" min="0" placeholder="Ingrese el monto aquí" bind:value={amount}>
+                    <input type="text" class="ipt" placeholder="Ingrese el monto aquí" bind:value={amount} on:focus={() => activeInput = 1}>
                 </div>
                 <p>{t('withdrawal.availableBalance')}: {user.balance} {user.currency}</p>
                 <p>{t('withdrawal.min')} {infoUser.retiro_min || 0} {user.currency} y {t('withdrawal.max')} {infoUser.retiro_max || 0} {user.currency}</p>
@@ -136,9 +157,9 @@
                     <!-- SOLO HAITI , SI EXISTEN MAS REFACTORIZAR TOO -->
                     <div class="withdrawal__phone">
                         <input type="telf" class="ipt" value="+509" disabled>
-                        <input type="text" inputmode="numeric" class="ipt" bind:value={infoAccount.numero_cta} on:input={inputAccountBank}>
+                        <input type="text" inputmode="numeric" class="ipt" bind:value={infoAccount.numero_cta} on:input={inputAccountBank} on:focus={() => activeInput = 2}>
                     </div>
-                    <input type="text" class="ipt" bind:value={infoAccount.adicional}>
+                    <input type="text" class="ipt" bind:value={infoAccount.adicional} on:focus={() => activeInput = 3}>
                     {:else}
                     <p>{t('profile.fullname')}</p>
                     <p>{t('profile.dni')}</p>
@@ -191,7 +212,27 @@
                 <a href="https://assets.apiusoft.com/{platform}/d&w.pdf" target="_blank">{@html t("withdrawal.termsConditions")}</a>
                 <div class="withdrawal__note">{@html t('withdrawal.reminderCompleteVerification')}</div>
                 {/if}
-
+                {#if typeView === "payMobile"}
+                <div class="tools__keyboard">
+                    <button class="btn keyboard" on:click={() => agregarNumero(1)}>1</button>
+                    <button class="btn keyboard" on:click={() => agregarNumero(2)}>2</button>
+                    <button class="btn keyboard" on:click={() => agregarNumero(3)}>3</button>
+                    <button class="btn keyboard" on:click={() => agregarNumero(4)}>4</button>
+                    <button class="btn keyboard" on:click={() => agregarNumero(5)}>5</button>
+                    <button class="btn keyboard" on:click={() => agregarNumero(6)}>6</button>
+                    <button class="btn keyboard" on:click={() => agregarNumero(7)}>7</button>
+                    <button class="btn keyboard" on:click={() => agregarNumero(8)}>8</button>
+                    <button class="btn keyboard" on:click={() => agregarNumero(9)}>9</button>
+                    <button class="btn keyboard"on:click={cleanPassword}>CE</button>
+                    <button class="btn keyboard" on:click={() => agregarNumero(0)}>0</button>
+                    <button class="btn keyboard" on:click={eliminarUltimoCaracter}>
+                        <svg width="100%" height="100%" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M11.7071 15.2938C11.3166 14.9033 11.3166 14.2701 11.7071 13.8796L13.5839 12.0027L11.7079 10.1267C11.3174 9.73617 11.3174 9.103 11.7079 8.71248C12.0984 8.32195 12.7316 8.32195 13.1221 8.71248L14.9982 10.5885L16.8796 8.70702C17.2702 8.3165 17.9033 8.3165 18.2938 8.70702C18.6844 9.09755 18.6844 9.73071 18.2938 10.1212L16.4124 12.0027L18.293 13.8833C18.6835 14.2739 18.6835 14.907 18.293 15.2975C17.9025 15.6881 17.2693 15.6881 16.8788 15.2975L14.9982 13.4169L13.1213 15.2938C12.7308 15.6843 12.0976 15.6843 11.7071 15.2938Z" />
+                            <path fill-rule="evenodd" clip-rule="evenodd" d="M5.6131 5.14653C6.18186 4.42266 7.05148 4 7.97206 4H20C21.6568 4 23 5.34315 23 7V17C23 18.6569 21.6568 20 20 20H7.97206C7.05148 20 6.18186 19.5773 5.6131 18.8535L1.68453 13.8535C0.829805 12.7656 0.829807 11.2344 1.68453 10.1465L5.6131 5.14653ZM7.97206 6C7.6652 6 7.37533 6.14089 7.18574 6.38218L3.25717 11.3822C2.97226 11.7448 2.97226 12.2552 3.25717 12.6178L7.18574 17.6178C7.37533 17.8591 7.6652 18 7.97206 18H20C20.5523 18 21 17.5523 21 17V7C21 6.44772 20.5523 6 20 6H7.97206Z" />
+                        </svg>
+                    </button>
+                </div>
+                {/if}
                 <button class="btn withdrawal" on:click={validateWithdrawal} disabled={loadWithdrawal}> 
                 {#if loadWithdrawal}
                     <div class="loading"><p></p><p></p><p></p></div>
