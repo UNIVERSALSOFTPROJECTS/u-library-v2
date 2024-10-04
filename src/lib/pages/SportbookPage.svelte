607491e3fd40ac0cf377e6f019abb8bd32bf425d
Dayana Horna <dayisitax@gmail.com>
2024-10-04 17:17:57 -0500

    update Live Sport Par

--- src/lib/pages/SportbookPage.svelte
+++ src/lib/pages/SportbookPage.svelte
@@ -34,7 +34,7 @@
   const guestURLbetw3LJLive ="https://betsamerica.betsw3.win/#/sport/?lang=spa&AuthToken=78140-504cb28edad6ad2f4fcff5d0b4b9&type=1"
   const guestURLbetw3PRLS = "https://sports.jcasino.live/?target=86224&name=parleysport#/?target=parleysport-86224-integration&token=123456789";
   const guestURLbetw3 = "https://sports.jbets.online/?target=86224&name=bets365#/?target=bets365-86224-integration&token=123456789";
-  const guestURLbetw3LivePRLS = "https://sports.jcasino.live/?target=86224&name=parleysport#/?target=parleysport-86224-integration&token=123456789";
+  const guestURLbetw3LivePRLS = "https://sports.jcasino.live/?target=86224&name=parleysport#/live_ecuabets/?target=parleysport-86224-integration&token=123456789";
   const guestURLbetw3Live = "https://sports.jbets.online/?target=86224&name=bets365#/live_ecuabets/?target=bets365-86224-integration&token=123456789";
   const guestURLpinnacle = "https://wngcxtx.oreo88.com/en/standard/home"
   const guestURLdigtain = `${GAMEAPI_URL}/e-digtain/init?t=-&gameid=${edg_id}&m=${deviceiframe}&skin=generic&`;
