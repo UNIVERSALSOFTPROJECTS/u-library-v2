db3a217813efc0fbc81f0d0a4acf1d8f4142d6da
Dayana Horna <dayisitax@gmail.com>
2024-10-23 15:22:52 -0500

    update url;

--- src/js/server.js
+++ src/js/server.js
@@ -289,7 +289,7 @@ const ServerConnection = (() => {
     const u_game = {
         getGameURLTest: (gameapi_url, game, usertoken) => {
             let url = gameapi_url + `/launch?gameid=${game.id}&p=${game.provider}&m=${game.mode}&sessionid=${usertoken}`;
-            return axios.get(url, {headers});
+            return url;
         }
     }
     /* */
