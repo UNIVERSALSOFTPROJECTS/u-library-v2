59e43e05335bd2ab59193fab476a2ca3693abc02
Dayana Horna <dayisitax@gmail.com>
2024-11-21 14:15:48 -0500

    update

--- src/lib/pages/SportbookFirstPage.svelte
+++ src/lib/pages/SportbookFirstPage.svelte
@@ -35,9 +35,9 @@
   }
 
   const openFirst = async () => { 
-    let url =userState == "loggedIn"? ut.getGameURL(GAMEAPI_URL, games.first, options.gameToken) : guestURLfirst;
+    let url = userState == "loggedIn"? ut.getGameURL(GAMEAPI_URL, games.first, options.gameToken) : guestURLfirst;
     if (userState == "loggedIn"){
-      const {data} = await backend.game.getURL(url);
+      const data = await backend.game.getURL(url);
       url = data.url
     }
     console.log("urlSportBook: ",url);
