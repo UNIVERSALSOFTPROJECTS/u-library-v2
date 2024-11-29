8b25c4d07052fa80f4886cd86c1c7bed56908bcb
Dayana Horna <dayisitax@gmail.com>
2024-11-29 10:44:16 -0500

    update params

--- src/lib/profileU/History.svelte
+++ src/lib/profileU/History.svelte
@@ -24,15 +24,11 @@
   };
 
   onMount(() => {
-    console.log("user", user);
     let actualDate = moment();
     let dateRest = actualDate.subtract(0, "days");
     filters.from = dateRest.format("YYYY-MM-DD");
     filters.to = moment().format("YYYY-MM-DD");
     promise = getMovements();
-    filters.playerId = user.playerId;
-    console.log("Filters", filters);
-    
   });
 
   const convertDateTimeZone = (lfecha) => {
@@ -62,7 +58,8 @@
   const getMovements = async () => {
     if(filters.type == "") delete filters.type;
     try { let params = {
-        ...filters, 
+        ...filters,
+        playerId: user.playerId,
         token:user.token
       }
       console.log("params1", params);
