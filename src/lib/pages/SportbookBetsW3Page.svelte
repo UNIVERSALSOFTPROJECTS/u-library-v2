2b75fd33a5d496b963170afe6c7cd63e56ae7ac3
Dayana Horna <dayisitax@gmail.com>
2025-01-03 11:43:01 -0500

    add url betsW3

--- src/lib/pages/SportbookBetsW3Page.svelte
+++ src/lib/pages/SportbookBetsW3Page.svelte
@@ -20,48 +20,62 @@
  
     const bw3_id = "betsw3_2024";
     
-    const guestURLbetw3bt1Live="https://sports.jcasino.live/#/live_ecuabets/?target=betsson1-86224-integration&token=123456789";
-    const guestURLbetw3bt1="https://sports.jcasino.live/#/?target=betsson1-86224-integration&token=123456789";
+    const guestURLbt1Live="https://sports.jcasino.live/#/live_ecuabets/?target=betsson1-86224-integration&token=123456789";
+    const guestURLbt1="https://sports.jcasino.live/#/?target=betsson1-86224-integration&token=123456789";
 
-    const guestURLbetw3GWLive="https://sports.jcasino.live/?target=86224&name=betsson3#/?target=betsson3-86224-integration&view=live";
-    const guestURLbetw3GW="https://sports.jcasino.live/?target=86224&name=betsson3#/?target=betsson3-86224-integration&view=prematch";
+    const guestURLPOIILive="https://sports.jcasino.live/?target=86224&name=betsson3#/?target=betsson3-86224-integration&view=live";
+    const guestURLPOII="https://sports.jcasino.live/?target=86224&name=betsson3#/?target=betsson3-86224-integration&view=prematch";
 
-    const guestURLbetw3LJLiveMobile ="https://mbetsamerica.betsw3.win/live/Soccer/?auth_token=78140-504cb28edad6ad2f4fcff5d0b4b9";
-    const guestURLbetw3LJMobile= "https://mbetsamerica.betsw3.win/prematch/Soccer/?auth_token=78140-504cb28edad6ad2f4fcff5d0b4b9";
+    const guestURLPROW = "https://sports.jcasino.live/?target=86224&name=betsson2#/?target=betsson2-86224-integration&view=live";
+    const guestURLPROWLive = "https://sports.jcasino.live/?target=86224&name=betsson2#/?target=betsson2-86224-integration&view=live";
 
-    const guestURLbetw3LJ = "https://betsamerica.betsw3.win/#/sport/?lang=spa&auth_token=78140-504cb28edad6ad2f4fcff5d0b4b9&type=0";
-    const guestURLbetw3LJLive ="https://betsamerica.betsw3.win/#/sport/?lang=spa&auth_token=78140-504cb28edad6ad2f4fcff5d0b4b9&type=1";
+    const guestURLPROG = "https://sports.jcasino.live/?target=86224&name=betsamerica#/?target=betsamerica-86224-integration";
+    const guestURLPROGLive = "https://sports.jcasino.live/?target=86224&name=betsamerica#/?target=betsamerica-86224-integration&view=live";
 
-    const guestURLbetw3 = "https://sports.jbets.online/?target=86224&name=bets365#/?target=bets365-86224-integration&token=123456789";
-    const guestURLbetw3Live = "https://sports.jbets.online/?target=86224&name=bets365#/live_ecuabets/?target=bets365-86224-integration&token=123456789";
+    const guestURLLJLiveMobile ="https://mbetsamerica.betsw3.win/live/Soccer/?auth_token=78140-504cb28edad6ad2f4fcff5d0b4b9";
+    const guestURLLJMobile= "https://mbetsamerica.betsw3.win/prematch/Soccer/?auth_token=78140-504cb28edad6ad2f4fcff5d0b4b9";
+
+    const guestURLLJ = "https://betsamerica.betsw3.win/#/sport/?lang=spa&auth_token=78140-504cb28edad6ad2f4fcff5d0b4b9&type=0";
+    const guestURLLJLive ="https://betsamerica.betsw3.win/#/sport/?lang=spa&auth_token=78140-504cb28edad6ad2f4fcff5d0b4b9&type=1";
+
+    const guestURL = "https://sports.jbets.online/?target=86224&name=bets365#/?target=bets365-86224-integration&token=123456789";
+    const guestURLLive = "https://sports.jbets.online/?target=86224&name=bets365#/live_ecuabets/?target=bets365-86224-integration&token=123456789";
 
     const guestURLs = {
-        BPEN: {
-            live: guestURLbetw3Live,
-            default: guestURLbetw3,
-        },
-        BUSD: {
-            live: guestURLbetw3Live,
-            default: guestURLbetw3,
-        },
-        GAWN: {
-            live: guestURLbetw3GWLive,
-            default: guestURLbetw3GW,
-        },
-        PRLS: {
-            live: guestURLbetw3bt1Live,
-            default: guestURLbetw3bt1,
+      BPEN: {
+        live: guestURLLive,
+        default: guestURL,
+      },
+      BUSD: {
+        live: guestURLLive,
+        default: guestURL,
+      },
+      POII: {
+        live: guestURLPOIILive,
+        default: guestURLPOII,
+      },
+      PROW:{
+        live: guestURLPROWLive,
+        default: guestURLPROW,
+      },
+      PROG: {
+        live: guestURLPROGLive,
+        default: guestURLPROG,
+      },
+      PRLS: {
+        live: guestURLbt1Live,
+        default: guestURLbt1,
+      },
+      default: {
+        live: {
+          mb: guestURLLJLiveMobile,
+          wb: guestURLLJLive,
         },
         default: {
-            live: {
-                mb: guestURLbetw3LJLiveMobile,
-                wb: guestURLbetw3LJLive,
-            },
-            default: {
-                mb: guestURLbetw3LJMobile,
-                wb: guestURLbetw3LJ,
-            },
+          mb: guestURLLJMobile,
+          wb: guestURLLJ,
         },
+      },
     };
 
     const resolveGuestURL = (clientCode, view, mode) => {
@@ -100,25 +114,25 @@
     }
 
     const openBetsW3 = async () => {
-        let url;
-        console.log(mode, "mode");
-        try {
-            if (userState === "loggedIn") {
-                url = ut.getGameURL(GAMEAPI_URL, games.BetW3, options.gameToken);
-                url += active_view === "sportbooklive" ? "&sport_view=live" : "&sport_view=sport";
-                url += `&lang=${lang}&r=url`;
-                const data = await backend.game.getURL(url);
-                url = data.url;
-            } else {
-                url = resolveGuestURL(CLIENT_CODE, active_view, mode);
-              }
-              if (!domain.includes('terminal')) {
-                RESELLER();
-              }
-            sportbookGameUrl = url;
-        } catch (error) {
-           console.log("Sportbook Error",error);  
+      let url;
+      console.log(mode, "mode");
+      try {
+        if (userState === "loggedIn") {
+          url = ut.getGameURL(GAMEAPI_URL, games.BetW3, options.gameToken);
+          url += active_view === "sportbooklive" ? "&sport_view=live" : "&sport_view=sport";
+          url += `&lang=${lang}&r=url`;
+          const data = await backend.game.getURL(url);
+          url = data.url;
+        } else {
+            url = resolveGuestURL(CLIENT_CODE, active_view, mode);
         }
+        if (!domain.includes('terminal')) {
+            RESELLER();
+          }
+        sportbookGameUrl = url;
+      } catch (error) {
+          console.log("Sportbook Error",error);  
+      }
     };
 
 
