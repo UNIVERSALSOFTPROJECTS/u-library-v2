5744d43ae99820d226418eca1a89d6b1a05241e3
Dayana Horna <dayisitax@gmail.com>
2025-02-11 16:54:51 -0500

    update logs

--- src/js/UserHelper.js
+++ src/js/UserHelper.js
@@ -9,7 +9,7 @@ const UserHelper = (() => {
         console.log("USER", u);
         if (u) {
             let user_ = JSON.parse(u);
-            console.log("userLooged", user_);
+            console.log("userLooged======>", user_);
             user = user_;
             let data;
             if (conf.CLIENT_CODE == 'JU02') {
@@ -18,8 +18,10 @@ const UserHelper = (() => {
             else {
                 data = await ServerConnection.users.getBalance(user.agregatorToken);
             }
+            console.log("userLooged CONF antes del websocket1 =============>", user);
+
             user.balance = data.data.balance;
-            //console.log("userLooged CONF", conf);
+            console.log("userLooged CONF antes del websocket=============>", conf);
             connectToLobbySocket(user, conf);
         }
         console.log("user", user);
@@ -27,7 +29,7 @@ const UserHelper = (() => {
     };
 
     const connectToLobbySocket = (user, conf) => {
-        console.log("CONGIH", conf);
+        console.log("CONGIH============>", conf, user);
         if (!conf.CLIENT_CODE) throw "CONF_CLIENT_CODE_NOT_FOUND";
         SocketConnector.connectToLobbySocket(`${conf.CLIENT_CODE}-${user.username}-${user.serial}`, conf); //conecta al websocket.
     };
