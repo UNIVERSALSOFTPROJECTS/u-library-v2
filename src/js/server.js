18eef35c88d2f0c03d67d577829452370e3a03ca
SteveVR <stevevaca2020@gmail.com>
2025-05-20 10:23:51 -0500

    update test login server

--- src/js/server.js
+++ src/js/server.js
@@ -85,7 +85,10 @@ const ServerConnection = (() => {
             return axios.post(url, payload, { headers });
         },
         login: (username, password, userType,turnstileToken = null) => {
-            console.log("conf",conf);
+            if (conf) {
+                
+                console.log("conf",conf);
+            }
             
             // if (!conf.org) throw "ORG_MANDATORY";
             let payload = { username, password, org: conf.org, userType }
