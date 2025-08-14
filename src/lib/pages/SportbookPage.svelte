f07da9de615fe88381d65a711bd26f498b9c7204
UniversalsoftPM <productmanagementup@gmail.com>
2025-08-14 11:23:46 -0500

    linkreplace

--- src/lib/pages/SportbookPage.svelte
+++ src/lib/pages/SportbookPage.svelte
@@ -49,7 +49,7 @@
   const guestURLbetw3LAUS = "https://sports2.jcasino.live/?target=86224&name=latinsport21#/?target=latinsport21-86224-integration&token=123456789";
   const guestURLbetw3LAUSlive = "https://sports2.jcasino.live/?target=86224&name=latinsport21#/live_ecuabets/?target=latinsport21-86224-integration&token=123456789"
   const guestURLbetw3GBPE = "https://sports2.jcasino.live/#/?target=ganabet365-86224-integration&token=123456789"
-  const guestURLbetw3GBPELive = "https://sports2.jcasino.live/#/live_ecuabets?target=ganabet365-86224-integration&token=123456789"
+  const guestURLbetw3GBPELive = "https://sports2.jcasino.live/#/?target=win365-86224-integration&token=123456789&view=live"
   const guestURLbetwBPEN = "https://sports2.jcasino.live/#/?target=win365-86224-integration&token=123456789"
   const guestURLbetwBPENLive = "https://sports2.jcasino.live/#/live_ecuabets/?target=win365-86224-integration&token=123456789"
 
