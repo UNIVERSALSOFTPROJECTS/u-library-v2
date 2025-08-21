e3b697206fff8d5e96c3727542ddf8c1447520f6
steve-vr <stevevaca2020@gmail.com>
2025-08-21 17:07:13 -0500

    update

--- src/lib/Banners.svelte
+++ src/lib/Banners.svelte
@@ -7,8 +7,8 @@
     export let onCategoryChange;
     export let typeCarrusel;
 
-    const urlJSON = 'https://d1dkqs4jlldj3s.cloudfront.net/generic_imgs/configBanners.json';
-    let bannerDefault = [{"url_w":"https://d1dkqs4jlldj3s.cloudfront.net/Latinsport21/bn_w_caballos2.png","url_m":"https://assets.apiusoft.com/Latinsport21/bn_m_caballos.png","dateFrom": "","dateUntil": "","category":"horses"}];
+    const urlJSON = 'https://assets.apiusoft.com/generic_imgs/configBanners.json';
+    let bannerDefault = [{"url_w":"https://d1dkqs4jlldj3s.cloudfront.net/Latinsport21/bn_w_caballos2.png","url_m":"https://d1dkqs4jlldj3s.cloudfront.net/Latinsport21/bn_m_caballos.png","dateFrom": "","dateUntil": "","category":"horses"}];
     let bannersJSON = [];
     let filteredBanners = [];
     let bannersLoading = true;
