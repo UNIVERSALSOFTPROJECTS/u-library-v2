6949e534038159205adf3542cb0ae083cec543c1
SteveVR <stevevaca2020@gmail.com>
2025-09-13 08:34:26 -0500

    update

--- src/js/server.js
+++ src/js/server.js
@@ -11,7 +11,7 @@ const ServerConnection = (() => {
         conf = config;
         headers = {
             "Content-Type": "application/json;charset=UTF-8", 
-            "clientAuth": conf.CLIENT_AUTH, "client": conf.CLIENT_CODE, ...(conf["x-tenant"] ? { "X-Tenant": conf["x-tenant"] } : {}),
+            "clientAuth": conf.CLIENT_AUTH || localStorage.getItem("CLIENT_AUTH"), "client": conf.CLIENT_CODE || localStorage.getItem("CLIENT_CODE"), ...(conf["x-tenant"] ? { "X-Tenant": conf["x-tenant"] } : {}),
             "Access-Control-Allow-Origin": "*",
             "Access-Control-Allow-Methods": "*",
             "Access-Control-Allow-Headers": "*",
