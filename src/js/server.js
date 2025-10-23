abda9683329eeca07776dfaf2036b3135519db0f
jordiValverde <55800542+JordiValverde@users.noreply.github.com>
2025-10-23 11:36:24 -0500

    Update server.js

--- src/js/server.js
+++ src/js/server.js
@@ -302,7 +302,7 @@ const ServerConnection = (() => {
         login: (username, password) => {
             let payload = { username, password }
             console.log("headers", headers);
-            return axios.post(conf.API + "/ol/auth/login", payload, { headers });
+            return axios.post(conf.API + "/ol/auth/login", payload/*, { headers }*/);
 
         },
         register: (payload) => {
