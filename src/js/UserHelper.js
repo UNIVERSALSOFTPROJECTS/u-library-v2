08097eb042803ca51ed56442aefb6490ffb0ed24
fridaluz22 <fridaluzf14@gmail.com>
2025-12-10 19:13:16 -0500

    socket

--- src/js/UserHelper.js
+++ src/js/UserHelper.js
@@ -31,7 +31,7 @@ const UserHelper = (() => {
         }
 
         if(user.type === "TERMINAL" && user.cashier){
-            connectToLobbySocketCashier(user, conf)
+            connectToLobbySocketTerminal(user, conf)
             initSocketEvents(onOpenNotification, user.cashier)
         }
 
@@ -53,7 +53,13 @@ const UserHelper = (() => {
         if (!conf.CLIENT_CODE) throw "CONF_CLIENT_CODE_NOT_FOUND";
         const serial = user.serial || user.aggregator_token?.slice(0,13);
         
-        SocketConnector.connectToLobbySocketCashier(`${conf.CLIENT_CODE}-${user.username}-${serial}`,`${user.cashier}-${user.serialCashier}`, conf);
+        SocketConnector.connectToLobbySocketCashier(`${conf.CLIENT_CODE}-${user.username}-${serial}`, conf);
+    };
+    const connectToLobbySocketTerminal = (user, conf) => {
+        if (!conf.CLIENT_CODE) throw "CONF_CLIENT_CODE_NOT_FOUND";
+        const serial = user.serial || user.aggregator_token?.slice(0,13);
+        
+        SocketConnector.connectToLobbySocketCashier(`${conf.CLIENT_CODE}-${user.username}-${serial}`, `${user.cashier}-${user.cashierSerial}`, conf);
     };
     const initSocketEvents = (onOpenNotification, currentcashier)=>{
         EventManager.subscribe("CASHIER_DISCONNECT", ({cashier})=>{
