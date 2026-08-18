f9cd9c9e13a98fef1b834e9a8d67c9bcbab6a27d
ArioMasgo <ario.masgo99@gmail.com>
2026-08-18 18:00:40 -0500

    update

--- src/js/server.js
+++ src/js/server.js
@@ -438,8 +438,9 @@ const ServerConnection = (() => {
             return axios.post(conf.API + "/api/casino/forgotPassword", payload_)
         },
         generateSignatureToOrderPayIn:async (payload,usertoken_nc) => {
-            return await axios.post("http://localhost:5000" + "/webhooks/payment/nxpay/signature", payload,{
+            return await axios.post(conf.API_WALLET + "/webhooks/payment/nxpay/signature", payload,{
                                                 headers: {
+                                                "Content-Type": "application/json;charset=UTF-8",
                                                 "x-token": usertoken_nc,
                                                 "X-Client-Code": conf.CLIENT_CODE,
                                                 "Accept": "*",
