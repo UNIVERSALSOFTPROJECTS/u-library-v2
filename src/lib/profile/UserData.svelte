3ef6665ed6f04fa3fabaffc51fb14211a34d1c88
ArioMasgo <ario.masgo99@gmail.com>
2026-09-21 10:52:12 -0500

    update

--- src/lib/profile/UserData.svelte
+++ src/lib/profile/UserData.svelte
@@ -86,9 +86,9 @@
         {#if !accountUser.isViewWeb}<p></p>{:else}<p>{t("profile.typeDoc")}</p>{/if}
         <input class="ipt" type="text" bind:value={accountUser.phone} disabled>
         {#if accountUser.isViewWeb}
-            {#if accountUser.doctype}
+            <!--{#if accountUser.doctype}-->
             <input class="ipt" type="text" bind:value={accountUser.doctype} disabled>
-            {:else}
+<!--            //{:else}-->
             <select class="slc" bind:value={selectDoctype}>
                 {#each doctypes as doctype}
                     <option value={doctype}>{doctype}</option>
